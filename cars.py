613b311e3d5f025930a4bfe26d5f44c15a7dd85a
seperina <seperina@seperinadeMacBook-Pro.local>
2019-07-06 00:18:00 +0800

    add test for sort()/sorted()

---
+++ cars.py
@@ -0,0 +1,47 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse = True)
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the reverse sorted list:")
+print(sorted(cars, reverse = True))
+print("\nHere is original list:")
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is original list:")
+print(cars)
+print("\nHere is reverse print list:")
+cars.reverse()
+print(cars)
+
+print("List length is " + str(len(cars)))
+
+# Exercise 3-8/9/10
+places = ['china', 'usa', 'uk', 'japan', 'india']
+print("\nHere is my places list:")
+print(places)
+print("\nHere is the sorted places list:")
+print(sorted(places))
+print("\nHere is the sorted reverse list:")
+print(sorted(places, reverse=True))
+places.reverse()
+print("\nHere is the reverse display:")
+print(places)
+places.reverse()
+print("\nHere is my original list:")
+print(places)
+places.sort()
+print("\n here is my sorted list now")
+print(places)
+places.sort(reverse=True)
+print("\n Here is reverse sorted list now")
+print(places)
+
+
