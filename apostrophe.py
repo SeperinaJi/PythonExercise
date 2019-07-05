4170a19ebba55940768946d13b599014947468c2
seperina <seperina@seperinadeMacBook-Pro.local>
2019-07-05 23:03:51 +0800

    add character test use str()

---
+++ apostrophe.py
@@ -0,0 +1,36 @@
+message = "One of Python's strengths is its diverse community.\n"
+print(message)
+
+# Exercise 2-3 test variable, character add
+user_name = "Eric"
+print("Hello " + user_name + ", would you like to learn some Python today?\n")
+
+#Exercise 2-4 test upper, lower, title
+first_name = "seperina"
+last_name = "Ji"
+full_name = first_name + " " + last_name
+print(full_name.lower() + '\n')
+print(full_name.upper() + '\n')
+print(full_name.title() + '\n')
+
+#Exercise 2-5 / 2-6
+author_name = "Albert Einstein"
+slang = '"A person who never made a mistake never tried anything new."'
+print(author_name + " once said, " + slang + '\n')
+
+#Exercise 2-7
+name = " seperina "
+print(name + '\n')
+print(name.rstrip() + '\n')
+print(name.lstrip() + '\n')
+print(name.strip())
+
+# Integer conversion to string use str()
+age = 23
+print("Happy " + str(age) + "rd Birthday!\n")
+
+#Exercise 2- 8
+print(2 + 6)
+print(2 * 4)
+print(16 / 2)
+print(10 - 2)
