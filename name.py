d8d9418c044b40c0b360becf1468f26f71a3b4ce
seperina <seperina@seperinadeMacBook-Pro.local>
2019-07-05 22:28:33 +0800

    add character test for use strip()/ title()/

---
+++ name.py
@@ -0,0 +1,20 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "Lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+print("Hello " + full_name.title() + "!")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = ' python '
+#favorite_language = favorite_language.rstrip()
+#favorite_language = favorite_language.lstrip()
+favorite_language = favorite_language.strip()
+print("Hello" + favorite_language + "3")
