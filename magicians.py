ef7874304fa6751541db01f94402208a3a870063
seperina <seperina@seperinadeMacBook-Pro.local>
2019-07-13 23:21:00 +0800

    add for exercise

---
+++ magicians.py
@@ -0,0 +1,12 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't see your next trick, " + magician.title() + '.\n')
+
+print("Thank you, everyone.That was a great magic show!.\n")
+
+#Exercise 4-1 pizza
+pizzas = ['Domino', 'Hawaii', 'Bacon']
+for pizza in pizzas:
+    print("I like " + pizza + " pizza")
+print("i REALLY LOVE THE PIZZA!")
